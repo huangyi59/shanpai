c94cca0d14b498d6193031888de4114d6db3cd99
huangyi <huangyi@zoudeMacBook-Pro.local>
2019-11-04 21:16:01 +0800

    学习

---
+++ shanpai/app/src/main/java/com/jzkj/shanpai/study/GlideActivity.java
@@ -0,0 +1,16 @@
+package com.jzkj.shanpai.study.annotation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jzkj.shanpai.R;
+
+public class GlideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glide);
+    }
+
+}
---
+++ shanpai/app/src/main/java/com/jzkj/shanpai/study/android/MySqlite.java
@@ -0,0 +1,4 @@
+package com.jzkj.shanpai.study.android;
+
+public class MySqlite {
+}
